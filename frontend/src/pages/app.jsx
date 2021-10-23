9e6471d8387efb4f1bc648c1d63344a69c1e9909
Ruy Araujo <ruy.araujo@yahoo.com.br>
2021-10-23 19:53:42 -0300

    add viewport

--- frontend/src/pages/app.jsx
+++ frontend/src/pages/app.jsx
@@ -1,10 +1,16 @@
 import styles from "../styles/App.module.css";
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
+import Head from "next/dist/shared/lib/head";
 
 const App = (props) => {
   return (
     <>
+      <Head>
+        <title>Ponere Chat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
       <div className={styles.container}>
         <Sidebar />
         <Chat />
