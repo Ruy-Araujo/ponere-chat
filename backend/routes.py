39e4c08742bc7daa7a09acc86fa174d8738fd1e4
Ruy Araujo <ruy.araujo@yahoo.com.br>
2021-10-29 12:11:54 -0300

    feat (login): implement login system

--- backend/routes.py
+++ backend/routes.py
@@ -1,36 +1,57 @@
-from backend import app
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+db = {
+    "users": [{"username": "demo", "password": "123"}]
+}
+
 
 @app.route("/sign-in", methods=['POST'])
 def sign_in():
     # Logic for sign_in
-    return "Login realizado com sucesso!", 200
+    req_user = request.json
+
+    for db_user in db["users"]:
+        if db_user["username"] == req_user["username"]:
+            if db_user["password"] == req_user["password"]:
+                return jsonify({"login": True}), 200
+            else:
+                return jsonify({"login": False, "error": "wrong password"}), 401
+
+    return jsonify({"login": False, "error": "username not exist"}), 404
+
 
 @app.route("/sign-out", methods=['POST'])
 def sign_out():
     # Logic for sign_out
     return "Você foi deslogado com sucesso!", 200
 
+
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
     # Logic for sign_up
     return "Cadastro realizado com sucesso!", 201
+
 
 @app.route("/friend", methods=['GET'])
 def list_friends():
     # Logic for get_friends
     return ({
         "friends": [
-                    "Ruy",
-                    "Igor",
-                    "Camilla",
-                    "Goya"
+            "Ruy",
+            "Igor",
+            "Camilla",
+            "Goya"
         ]
     }, 200)
+
 
 @app.route("/friend", methods=['POST'])
 def add_friend():
     # Logic for add_friend
     return "Amigo adicionado com sucesso!", 201
+
 
 @app.route("/friend/<friend_name>", methods=['GET'])
 def get_friend(friend_name):
@@ -75,7 +96,12 @@ def get_friend(friend_name):
         ]
     }, 200)
 
+
 @app.route("/message", methods=['POST'])
 def add_message():
     # Logic for add_message
     return "Mensagem cadastrada com sucesso!", 201
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', debug=True)
